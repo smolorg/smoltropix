9161680183423b38eab0a33770064cf0e4a5fc8b
AtakanTekparmak <atakantekerparmak@gmail.com>
2024-10-16 15:16:38 +0200

    fix: Extended gitignore, added missing req and fixed f-string issue in main

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ def main(input: str, weights_path: Path = DEFAULT_WEIGHTS_PATH.joinpath("1B-Inst
         logits, kvcache, _, _ = xfmr(xfmr_weights, model_params, tokens, cur_pos, freqs_cis[:seqlen], kvcache, attn_mask=attn_mask)
         next_token = mx.argmax(logits[:, -1], axis=-1, keepdims=True).astype(mx.int32)
         gen_tokens = next_token
-        rich.print(f"[{COLORS["lelv"]}]{tokenizer.decode([next_token.item()])}[/]", end='', flush=True)
+        rich.print(f"[{COLORS['lelv']}]{tokenizer.decode([next_token.item()])}[/]", end='', flush=True)
         cur_pos = seqlen
         stop = mx.array([128001, 128008, 128009])
         sampler_cfg = SamplerConfig()
